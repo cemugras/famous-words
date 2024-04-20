030577698b5ba1dff3509ea7d334cb05ced59ea5
emrebezci <emrebezci@protonmail.com>
2024-04-20 19:54:13 +0300

    first commit

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,12 +1,44 @@
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function App() {
+function NavScrollExample() {
   return (
-    <div className="App">
-      <h1>FAMOUS WORDS</h1>
-      <h2>TEST</h2>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              
+              
+              <NavDropdown.Divider />
+            </NavDropdown>
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
   );
 }
 
-export default App;
+export default NavScrollExample;
---
+++ fe/src/main.js
@@ -0,0 +1,64 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Row, Col } from 'react-bootstrap';
+
+
+function Main() {
+    return (
+        <Row>
+            <Col>
+                <Card style={{ width: '18rem', marginTop: '10px' }}>
+                    <Card.Img height={300} variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Ataturk1930s.jpg/444px-Ataturk1930s.jpg" />
+                    <Card.Body>
+                        <Card.Title>Mustafa Kemal ATATURK</Card.Title>
+                        <Card.Text>
+                            “Everything we see in the world is the creative work of women.”
+                        </Card.Text>
+                        <Button variant="primary">More Quotes from -&&&PERSON&&&-</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '18rem', marginTop: '10px' }}>
+                    <Card.Img height={300} variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Ataturk1930s.jpg/444px-Ataturk1930s.jpg" />
+                    <Card.Body>
+                        <Card.Title>Mustafa Kemal ATATURK</Card.Title>
+                        <Card.Text>
+                            “Everything we see in the world is the creative work of women.”
+                        </Card.Text>
+                        <Button variant="primary">More Quotes from -&&&PERSON&&&-</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '18rem', marginTop: '10px' }}>
+                    <Card.Img height={300} variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Ataturk1930s.jpg/444px-Ataturk1930s.jpg" />
+                    <Card.Body>
+                        <Card.Title>Mustafa Kemal ATATURK</Card.Title>
+                        <Card.Text>
+                            “Everything we see in the world is the creative work of women.”
+                        </Card.Text>
+                        <Button variant="primary">More Quotes from -&&&PERSON&&&-</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '18rem', marginTop: '10px' }}>
+                    <Card.Img height={300} variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Ataturk1930s.jpg/444px-Ataturk1930s.jpg" />
+                    <Card.Body>
+                        <Card.Title>Mustafa Kemal ATATURK</Card.Title>
+                        <Card.Text>
+                            “Everything we see in the world is the creative work of women.”
+                        </Card.Text>
+                        <Button variant="primary">More Quotes from -&&&PERSON&&&-</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+        
+        
+    );
+}
+
+export default Main;
+
