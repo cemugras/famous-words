878b9e623113d26c22047af582d8760dfb5bf6e2
emrebezci <emrebezci@protonmail.com>
2024-04-20 23:43:50 +0300

    navbar main update

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './main';
+import Main from './Main';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Main />
   </React.StrictMode>
 );
 
