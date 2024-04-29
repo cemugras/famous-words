cebc57a81e624f8b85a9b8cbf42d9ca01e1982da
cemugras <cemugrs@gmail.com>
2024-04-30 01:42:06 +0300

    frontend dynamic routing

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { AppProvider } from './components/context/AppContext';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import {AppProvider} from './components/context/AppContext';
 import Home from './components/home/Home';
 import Authors from './components/authors/Authors';
 import Topics from './components/topics/Topics';
@@ -9,20 +9,20 @@ import Navbar from './components/navbar/Navbar';
 import './App.css';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <AppProvider>
-        <Navbar />
-        <Routes>
-          <Route path='/home' element={<Home />}></Route>
-          <Route path='/authors' element={<Authors />}></Route>
-          <Route path='/author' element={<Author />}></Route>
-          <Route path='/topics' element={<Topics />}></Route>
-          <Route path='/' element={<Navigate to='/home' />} />
-        </Routes>
-      </AppProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppProvider>
+                <Navbar/>
+                <Routes>
+                    <Route path='/home' element={<Home/>}></Route>
+                    <Route path='/authors' element={<Authors/>}></Route>
+                    <Route path='/topics' element={<Topics/>}></Route>
+                    <Route path='/' element={<Navigate to='/home'/>}/>
+                    <Route path="/:name" element={<Author/>}/>
+                </Routes>
+            </AppProvider>
+        </BrowserRouter>
+    );
 };
 
 export default App;
