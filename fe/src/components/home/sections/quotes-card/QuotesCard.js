2e3635ba7cf6251549798ea362ed57141d04160b
emrebezci <emrebezci@protonmail.com>
2024-05-04 06:10:58 +0300

    card skeleton added and social icon changed

--- fe/src/components/home/sections/quotes-card/QuotesCard.js
+++ fe/src/components/home/sections/quotes-card/QuotesCard.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import SocialShare from '../social-share/SocialShare';
 import LikeButton from '../like-button/LikeButton';
+import Skeleton from './skeletonCard'
+
 const QuotesCard = ({data}) => {
   if (!data) {
-    return <div>Loading...</div>;
+    // Veri yüklenirken Skeleton gösterelim
+    return (
+      <div className='cards'>
+        {[...Array(5)].map((_, index) => (
+          <div key={index}>
+            <div className='card'>
+              <div style={{ display: 'flex', alignItems: 'center', marginBottom: '12px' }}>
+                <div style={{ marginRight: '10px', width: '50px', height: '50px' }}>
+                  <Skeleton />
+                </div>
+  
+                <div>
+                  <Skeleton />
+                </div>
+              </div>
+              
+              <Skeleton />
+  
+              <div className='social'>
+                <Skeleton />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
@@ -28,18 +55,16 @@ const QuotesCard = ({data}) => {
                 ></a>
               </div>
             </div>
-            {item.quotes.map((quote, index) => {
-              return (
-                <div key={index}>
-                  <p style={{ lineHeight: '1.5', fontSize: '1rem', marginBottom: '20px' }}>
-                    <i className='fa-solid fa-quote-left'></i> {quote} <i className='fa-solid fa-quote-right'></i>
-                  </p>
-                </div>
-              );
-            })}
+            
+            <div key={index}>
+              <p style={{ lineHeight: '1.5', fontSize: '1rem', marginBottom: '20px' }}>
+                <i className='fa-solid fa-quote-left'></i> {item.quotes[0]} <i className='fa-solid fa-quote-right'></i>
+              </p>
+            </div>
+            
             <div className='social'>
               <SocialShare />
-              <LikeButton />
+              
             </div>
           </div>
         </div>
---
+++ fe/src/components/home/sections/quotes-card/skeletonCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skeleton = () => {
+  return (
+    <div className="h-12 w-full bg-gray-200 animate-pulse mb-4 rounded-lg"></div>
+  );
+};
+
+
+
+export default Skeleton;
