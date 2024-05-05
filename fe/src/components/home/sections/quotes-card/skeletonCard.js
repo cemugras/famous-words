096e0620beb3cd0e8faf362df17942e8ebb0ebd4
emrebezci <emrebezci@protonmail.com>
2024-05-06 02:20:22 +0300

    Quotes card skeleton update

--- fe/src/components/home/sections/quotes-card/skeletonCard.js
+++ fe/src/components/home/sections/quotes-card/skeletonCard.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const Skeleton = () => {
   return (
-    <div className="h-12 w-full bg-gray-200 animate-pulse mb-4 rounded-lg"></div>
+    <div className="animate-pulse">
+      <div className="author-quotes">
+        <div className="quote-card">
+          <div className="animated-border-quote">
+            <blockquote>
+              <div className="card-header flex items-center">
+                <div className="skeleton-avatar h-10 w-10 rounded-full mr-4"></div>
+                <div className="skeleton-text h-4 w-24 bg-gray-300"></div>
+              </div>
+              <div className="skeleton-text h-4 w-full bg-gray-300"></div>
+              <div className="skeleton-text h-4 w-full bg-gray-300"></div>
+              <div className="social">
+                {/* Skeleton for social share */}
+                <div className="flex items-center">
+                  <div className="skeleton-text h-4 w-12 bg-gray-300"></div>
+                  <div className="skeleton-text h-4 w-12 bg-gray-300 ml-2"></div>
+                </div>
+              </div>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
