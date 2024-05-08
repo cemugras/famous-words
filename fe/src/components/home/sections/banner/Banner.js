bd19dd2364e089e85b21beea91a6e4f98c1bfd14
kafeincugras <cem.ugras@vodafone.com>
2024-05-08 03:37:40 +0300

    firebase deployment test commit

--- fe/src/components/home/sections/banner/Banner.js
+++ fe/src/components/home/sections/banner/Banner.js
@@ -33,7 +33,6 @@ const Banner = () => {
   }
 
   const getRandomItems = (array, numberOfItems) => {
-    // Diziyi kopyalayın ve karıştırın
     const shuffledArray = [...array].sort(() => Math.random() - 0.5);
 
     return shuffledArray.slice(0, numberOfItems);
