c54ce3b0c88a6ab4fda5fc9ff482a8a57f99ac7d
cemugras <cemugrs@gmail.com>
2024-05-09 21:08:02 +0300

    favicon, homepage title and logo refactored

--- fe/src/components/navbar/Navbar.js
+++ fe/src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../../assets/famous-quotes-logo.png';
+import Logo from '../../assets/quotes-cyclopedia-logo.png';
 import SearchBox from '../search-box/SearchBox';
 
 const Navbar = () => {
